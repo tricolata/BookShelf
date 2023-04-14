df3160a11636c1c72476828298e20da4e265d9ce
ykubrom <yafet.kubrom@gmail.com>
2023-04-13 23:19:22 -0500

    initial spring commit

---
+++ src/main/java/com/springboot/bookshelfapp/BookShelfAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.bookshelfapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShelfAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShelfAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/bookshelfapp/BookShelfAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.bookshelfapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShelfAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
